abc53634843ab33f32ee3ed0b99435cc8c1ed339
github username <github email address>
2018-10-04 17:54:55 -0400

    Final Project

--- app/api.rb
+++ app/api.rb
@@ -23,7 +23,7 @@ def cat_count
     total_arr
 end
 
-total_arr = cat_count
+#total_arr = cat_count
 
 #--------------Randomly select two instances for each category-----------------#
 
@@ -44,7 +44,7 @@ def rand_numbers(total_arr)
 end
 num_arr
 end
-randnums = rand_numbers(total_arr)
+#randnums = rand_numbers(total_arr)
 
 #-----------------To input valid starship numbers.-------------------#
 
@@ -60,10 +60,7 @@ y = ship_arr.sample
 randnums = randnums.fill(x.to_i, 3..3)
 randnums = randnums.fill(y.to_i, 8..8)
 end
-nums = valid_nums(randnums)
-
-binding.pry
-
+#nums = valid_nums(randnums)
 
 #--------------Randomly select two instances for each category-----------------#
 
@@ -92,39 +89,90 @@ end # end of method
 
 #subjects = get_subjects(nums)
 
+#---------------------Agregating Subject Methods-------------------------------#
 
-
-#-----------------------------------Agregating Solutions---------------------------#
-
-def solutions()
+def qs_about()
   total_arr = cat_count
   randnums = rand_numbers(total_arr)
   nums = valid_nums(randnums)
   subjects = get_subjects(nums)
-
-  # people_arr = (1..87).to_a
-  # people_arr << nums[0]
-  # people_arr << nums[5]
-  # p_arr = people_arr.uniq
-  # binding.pry
+  $nums = nums
 end
-x = solutions
+
+x = qs_about
+
+#-------------------------------Selecting Wrong Choices------------------------#
+
+def wchoices
+wc_arry = []
+#Q1-------Characters-------#
+    c_arr = (1..87).to_a
+    c_arr.delete($nums[0])
+    c_arr.delete($nums[5])
+    c_wc = c_arr.sample(3)
+    wc_arry << c_wc
+#Q2------Films-------------#
+    f_arr = (1..7).to_a
+    f_arr.delete($nums[1])
+    f_arr.delete($nums[6])
+    f_wc = f_arr.sample(3)
+    wc_arry << f_wc
+#Q3------Planets-------------#
+    pl_arr = (1..61).to_a
+    pl_arr.delete($nums[2])
+    pl_arr.delete($nums[7])
+    pl_wc = pl_arr.sample(3)
+    wc_arry << pl_wc
+#Q4------Starships-------------#
+    st_arr = [2, 3, 5, 9, 10, 11, 12, 13, 15, 17, 21, 22,
+              23, 27, 28, 29, 31, 32]
+    st_arr.delete($nums[3])
+    st_arr.delete($nums[8])
+    st_wc = st_arr.sample(3)
+    wc_arry << st_wc
+#Q5------Species-------------#
+    sp_arr = (1..37).to_a
+    sp_arr.delete($nums[4])
+    sp_arr.delete($nums[9])
+    sp_wc = sp_arr.sample(3)
+    wc_arry << sp_wc
+#Q6------Characters-------#
+    c_wc = c_arr.sample(3)
+    wc_arry << c_wc
+#Q7------Films-------------#
+    f_wc = f_arr.sample(3)
+    wc_arry << f_wc
+#Q8------Planets-------------#
+    pl_arr = (1..61).to_a
+    pl_arr.delete($nums[2])
+    pl_arr.delete($nums[7])
+    pl_wc = pl_arr.sample(3)
+    wc_arry << pl_wc
+#Q9------Starships-------------#
+    st_wc = st_arr.sample(3)
+    wc_arry << st_wc
+#Q10-----Species-------------#
+    sp_wc = sp_arr.sample(3)
+    wc_arry << sp_wc
+
+end
+
+wca = wchoices
 binding.pry
-  # pepole_arr = people_arr.delete_at(index(nums[0]))
-  # people_arr = people_arr.delete_at(index(nums[5]))
 
 
-  # people_arr = (1..87).to_a.index(nums[5])
-  # films_arr = (1..7).to_a.delete(nums[1]).delete(nums[6])
-  # planets_arr = (1..61).to_a.delete(nums[2]).delete(nums[7])
-  # starships arr = [2, 3, 5, 9, 10, 11, 12, 13, 15, 17, 21, 22,
-  #                  23, 27, 28, 29, 31, 32]to_a.delete(nums[3]).delete(nums[8])
-  # species_arr = (1..37).to_a.delete(nums[4]).delete(nums[9])
 
-# end
 
-#-----------------------------------Agregating Choices------------------------#
+# pepole_arr = people_arr.delete_at(index(nums[0]))
+# people_arr = people_arr.delete_at(index(nums[5]))
 
+
+# people_arr = (1..87).to_a.index(nums[5])
+# films_arr = (1..7).to_a.delete(nums[1]).delete(nums[6])
+# planets_arr = (1..61).to_a.delete(nums[2]).delete(nums[7])
+# starships arr = [2, 3, 5, 9, 10, 11, 12, 13, 15, 17, 21, 22,
+#                  23, 27, 28, 29, 31, 32]to_a.delete(nums[3]).delete(nums[8])
+# species_arr = (1..37).to_a.delete(nums[4]).delete(nums[9])
 
 
 #def choices(total_arr, rand_nums, nums, subjects)
